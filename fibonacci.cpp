23d864789eb3d70d15acb4b24e6ad39c3e35ed25
Hannan <hannanofx@gmail.com>
2024-08-31 07:52:52 +0500

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int fibo(int x) {
+	int n = 1, n1 = 1, n2 = 1, sum;
+        while (n <= x) {
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+		n++;
+	}
+		x = sum;
+		return x;
+}
+	int main() {
+		int a;
+		cout << "Enter the position:" << endl;
+		cin >> a;
+		cout << "The sum of position " << a << " is: " << fibo(a) << endl;
+		return 0;
+	}
+
+
